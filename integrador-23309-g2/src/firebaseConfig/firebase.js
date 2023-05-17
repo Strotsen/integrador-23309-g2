e80bd466fa50868d4f98a97dd6ad5c7000ec73cb
Veronica Strotsen <thestrotsen@gmail.com>
2023-05-17 18:38:55 -0300

    primer commit

---
+++ integrador-23309-g2/src/firebaseConfig/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQBU_WmcsE3Dd7zmdxLNETPpWAqLCsH9g",
+  authDomain: "integrador-23309-g2.firebaseapp.com",
+  projectId: "integrador-23309-g2",
+  storageBucket: "integrador-23309-g2.appspot.com",
+  messagingSenderId: "794001888434",
+  appId: "1:794001888434:web:7230dc1445f5f6714565ab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
