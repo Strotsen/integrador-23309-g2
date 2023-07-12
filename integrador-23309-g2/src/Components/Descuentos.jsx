5b6e2c3b90a99898ab8fa14d96ecd72ff5a832df
Hernán Novara <her.novara@hotmail.com>
2023-07-11 21:51:13 -0300

    Descuentos Carrousel

--- integrador-23309-g2/src/Components/Descuentos.jsx
+++ integrador-23309-g2/src/Components/Descuentos.jsx
@@ -8,42 +8,42 @@ export const Descuentos = () => {
 
   return (
 
+    
+
     <Carousel>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="./img/a1.jpeg"
+          src="./a1.jpeg"
           alt="30% descuento"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3></h3>
+          <p></p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="./img/a2.jpeg"
+          src="./a2.jpeg"
           alt="50% descuento"
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3></h3>
+          <p></p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="./img/a3.jpeg"
+          src="./a3.jpeg"
           alt="10% descuento"
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <h3></h3>
+          <p></p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
